37b83444d43c18081b76091041acb2cc578a520e
Josh W <wilson.jd022@gmail.com>
2023-11-07 22:21:16 -0800

    Moving forward

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Day 25
 
-## Objective:
-Csv files in python.
+### Objectives:
+- Learn how to handle Csv files in python.
+- Learn how to use pandas at a basic level
+- Get used to dataframes and series
+
+### Expected Outcome:
+- Be able to work with basic csv files
+- Create new csv files
+- correctly parse and modify data.
+- basic data analysis.
--- main.py
+++ main.py
@@ -21,5 +21,36 @@ import pandas
 
 if __name__ == '__main__':
     data = pandas.read_csv("weather_data.csv")
-    print(data["temp"])
+    # print(data["temp"])
+    # We can get this well formatted dataframe into a data dict
+    data_dict = data.to_dict()
+    # print(data_dict)
+    temperature_list = data["temp"].to_list()
+    print(temperature_list)
+    # Task: Find the average of the temperatures
+    print(sum(temperature_list) / len(temperature_list))
+    # There is another way with just pandas
+    print(data["temp"].mean())
+    # Task: Obtain the max for temperatures
+    print(data["temp"].max())
+    # Get data from columns
+    print(data.day)
+    # Get Data from rows
+    print(data[data.day == "Monday"])
+    # Task: Obtain the max row with the max temperature
+    print(data[data.temp == data.temp.max()])
+    # To retrieve a specific column for a specified row
+    print(data[data.day == "Monday"].condition)
+    # Task: convert temps from C -> F
+    print([ (x * (9/5)) + 32 for x in data.temp.tolist()])
+    # Create a dataframe from scratch
+    data_dic = {
+        "students": ["James", "Fred", "Amy", "Jenny"],
+        "scores": [6, 2, 10, 8]
+    }
+    data = pandas.DataFrame(data_dic)
+    print(data)
+#   We can also take this and create a csv file
+    data.to_csv("new.csv")
+
 
